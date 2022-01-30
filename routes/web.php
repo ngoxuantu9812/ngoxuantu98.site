c01d1b26d56616ba2134ea0468313ee947fc9485
Xuân Tú Ngô <ngoxuantu9812@gmail.com>
2022-01-30 23:17:29 +0700

    Update code

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(
+    
     ['prefix' => '/{lang?}/', 'where' => ['lang' => 'vi|en']],
     function () {
         Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
